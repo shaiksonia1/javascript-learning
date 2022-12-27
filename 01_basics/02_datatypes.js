9e94c03cab47880e863d159b7c877b9c23b53abb
Shaik Sonia <79378738+shaiksonia1@users.noreply.github.com>
2022-12-27 21:30:29 +0000

    commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) we are using nodejs, not browser
+
+
+let name="sonia"
+let age = 18
+let isLoggedIn = false
+
+/*
+
+primitive datatypes
+
+number => 2 to the power 53
+bigint => to store the bigger number 
+string => "hello"
+boolean => true or false
+null => standalone value
+undefined => value is not defined for example "let state;"
+symbol =>unique 
+
+object
+*/
+
+console.log(typeof "sonia"); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
